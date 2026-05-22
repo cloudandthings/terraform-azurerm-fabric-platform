77842ad37741b470e2b79cf55a515f9d838e8b1b
JannoCAT <janno@cloudandthings.io>
2026-05-22 10:42:10 +0200

    chore: 2026-05-22 Get repo terraform registry ready

--- provider.tf
+++ provider.tf
@@ -19,18 +19,3 @@ terraform {
     }
   }
 }
-
-provider "fabric" {
-  tenant_id = var.fabric_provider.tenant_id
-  use_cli   = true
-  preview   = true
-}
-
-provider "azurerm" {
-  features {}
-  subscription_id = var.fabric_provider.subscription_id
-}
-
-provider "azuread" {
-  tenant_id = var.fabric_provider.tenant_id
-}
